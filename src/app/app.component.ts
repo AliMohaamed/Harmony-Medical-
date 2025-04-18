d5a95148505997d4a765616abc8843f235833f3e
Ali Mohamed <alimohamed11907@gmail.com>
2025-04-18 18:10:39 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,39 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { AboutComponent } from './components/about/about.component';
+import { StatsComponent } from './components/stats/stats.component';
+import { ServicesComponent } from './components/services/services.component';
+import { AppointmentComponent } from './components/appointment/appointment.component';
+import { DepartmentsComponent } from './components/departments/departments.component';
+import { DoctorsComponent } from './components/doctors/doctors.component';
+import { QAComponent } from './components/qa/qa.component';
+import { TestimonialsComponent } from './components/testimonials/testimonials.component';
+import { GalleryComponent } from './components/gallery/gallery.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    HeroComponent,
+    AboutComponent,
+    StatsComponent,
+    ServicesComponent,
+    AppointmentComponent,
+    DepartmentsComponent,
+    DoctorsComponent,
+    QAComponent,
+    TestimonialsComponent,
+    GalleryComponent,
+    ContactComponent,
+    FooterComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'landing-page-angular';
---
+++ src/app/components/qa/qa.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-qa',
+  imports: [],
+  templateUrl: './qa.component.html',
+  styleUrl: './qa.component.css'
+})
+export class QAComponent {
+
+}
